17a3d007fced2f5688c8f943c7f0e4efc2757999
MissJuly <joygitau98@gmail.com>
2023-08-31 23:36:59 +0300

    Add tasak 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module for filtering logs
+"""
+import re
+from typing import List
+
+
+patterns = {
+    'extract': lambda x, y: r'(?p<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+    Filters a log line
+    """
+    extract, replace = (patterns['extract'], patterns['replace'])
+    return re.sub(extract(fields, separator), replace(redaction), message)
