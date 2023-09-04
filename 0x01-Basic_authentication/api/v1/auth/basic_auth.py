0bf731870aee4952074fb96ffb8396756c8ce7a5
MissJuly <joygitau98@gmail.com>
2023-09-05 00:41:42 +0300

    Add task 3, 4, 5, 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that contains class BasicAuth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Basic Authentication class
+    """
+    pass
