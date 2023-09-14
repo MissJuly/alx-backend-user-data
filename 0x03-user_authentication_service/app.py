c258e0522b16ee32b47045a46fe64fbcd079181c
MissJuly <joygitau98@gmail.com>
2023-09-15 01:56:23 +0300

    Add task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,8 @@
 """
 API Routes for authentication
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request, abort
 
 
 app = Flask(__name__)
@@ -12,6 +13,24 @@ app = Flask(__name__)
 def index() -> str:
     """Base route"""
     msg = {"message": "Bienvenue"}
+    return jsonify(msg)
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """Registers a new user"""
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = Auth.register_user(email, password)
+    except ValueError:
+        msg = {"message": "email already registered"}
+        return jsonify(msg), 400
+
+    msg = {"email": email, "message": "user created"}
     return jsonify(msg)
 
 
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,12 +4,19 @@ import bcrypt
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
 from db import DB
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> str:
     """Returns a salted hash of the input password"""
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
+
+
+def _generate_uuid() -> str:
+    """returns a string representation of a new uuid"""
+    UUID = uuid4()
+    return str(UUID)
 
 
 class Auth:
@@ -30,3 +37,28 @@ class Auth:
             return user
         else:
             raise ValueError(f'User (email) already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate login"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        user_password = user.hashed_password
+        encoded_password = password.encode()
+
+        if bcrypt.checkpw(encoded_password, user_password):
+            return True
+
+        return False
+
+    def create_session(self, email: str) -> str:
+        """Returns session ID for a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
