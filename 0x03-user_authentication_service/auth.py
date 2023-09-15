58960d77ebd4341a1614bacbb150ee93fb0234a1
MissJuly <joygitau98@gmail.com>
2023-09-15 23:16:20 +0300

    Add task 15

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,3 +88,16 @@ class Auth:
         self._db.update_user(user.id, session_id=None)
 
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """retrieves token from user"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        reset_token = _generate_uuid()
+
+        self._db.update_user(user.id, reset_token=reset_token)
+
+        return reset_token
